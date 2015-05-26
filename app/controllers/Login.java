db4390c30cdcd3a96f2d743d1467fb95c617d725
OCC Teacher <occ@example.com>
2015-05-26 17:19:05 +0900

    ログイン画面の修正

--- app/controllers/Login.java
+++ app/controllers/Login.java
@@ -1,14 +1,28 @@
 package controllers;
+import play.data.Form;
 
 import play.*;
 import play.mvc.*;
 
 import views.html.login.*;
 
+
 public class Login extends Controller {
+    public static class Logins {
+       public String SyainID;
+       public String PassWord;
+    }
+
+	 public static Form<Login> loginForm = Form.form(Login.class);
+
 
     public static Result index() {
-        return ok(index.render());
+        return ok(index.render(loginForm));
+    }
+    public static Result authenticate() {
+    	Login login = loginForm.bindFromRequest().get();
+    	return ok("ログインしたユーザは です");
+
     }
 
 }
