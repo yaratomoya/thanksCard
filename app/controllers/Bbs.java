e95e31f477cd93f7e668b49582370ccfbac5d4c4
TAKAZATO <TAKAZATO@10.0.87.135>
2015-05-27 01:01:44 +0900

    掲示板の検索オプションのフォームをフォームヘルパー形式に変更

--- app/controllers/Bbs.java
+++ app/controllers/Bbs.java
@@ -2,16 +2,22 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import play.data.Form;
 import views.html.bbs.*;
 
 public class Bbs extends Controller {
+	 public static Form<Bbs> bbsForm = Form.form(Bbs.class);
 
     public static Result index() {
-        return ok(index.render());
+        return ok(index.render(bbsForm));
     }
     public static Result lastMonth() {
-        return ok(lastMonth.render());
+        return ok(lastMonth.render(bbsForm));
+    }
+
+
+    public static Result search(){
+    	return ok("検索結果");
     }
 
 }
