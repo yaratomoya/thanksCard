c3a90575d85c86b22f7a71208cc6583d3b421593
Administrator <Administrator@user-pc>
2015-05-27 01:13:18 +0900

    感謝カードの内容を全部表示できた！

--- app/controllers/Bbs.java
+++ app/controllers/Bbs.java
@@ -1,14 +1,16 @@
 package controllers;
 
+import java.util.List;
+import models.ThanksCard;
 import play.*;
 import play.mvc.*;
-
 import views.html.bbs.*;
 
 public class Bbs extends Controller {
 
     public static Result index() {
-        return ok(index.render());
+    	List<ThanksCard> card=ThanksCard.find.all();
+        return ok(index.render(card));
     }
     public static Result lastMonth() {
         return ok(lastMonth.render());
