6fd597a317a269b9187ea311b100e6229c6a5d1f
OCC Teacher <occ@example.com>
2015-05-27 16:46:26 +0900

    routesの追加 メソッドの追加 ポップアップの追加

--- app/controllers/Bbs.java
+++ app/controllers/Bbs.java
@@ -12,6 +12,12 @@ public class Bbs extends Controller {
     	List<ThanksCard> card=ThanksCard.find.all();
         return ok(index.render(card));
     }
+
+    public static Result popup() {
+    	List<ThanksCard> card=ThanksCard.find.all();
+        return ok("日付 感謝された人（部署・名前）ヘルプカテゴリ してもらったこと 感謝の言葉 感謝された人（部署・名前）ＯＫボタン");
+    }
+
     public static Result lastMonth() {
         return ok(lastMonth.render());
     }
