7b16640d6b4776fab19e13bee44e8b7ee8a24b85
OCC Teacher <occ@example.com>
2015-05-27 16:09:05 +0900

    削除依頼完了のポップアップの表示を追加

--- app/controllers/Mydata.java
+++ app/controllers/Mydata.java
@@ -10,4 +10,8 @@ public class Mydata extends Controller {
     public static Result index() {
         return ok(index.render());
     }
+
+    public static Result popup() {
+        return ok("感謝カードの削除を依頼しました。");
+    }
 }
