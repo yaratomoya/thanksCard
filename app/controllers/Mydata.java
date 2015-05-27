e1a91ecc0b4c1791e9140ede201ee490cf3981bf
User <User@H27-NotePC05>
2015-05-27 17:56:22 +0900

    感謝カード一覧のテーブル更新

--- app/controllers/Mydata.java
+++ app/controllers/Mydata.java
@@ -1,13 +1,19 @@
 package controllers;
 
 import play.*;
+import play.data.Form;
 import play.mvc.*;
-
 import views.html.mydata.*;
 
 public class Mydata extends Controller {
+	public static Form<Mydata> mydataForm=Form.form(Mydata.class);
+
 
     public static Result index() {
-        return ok(index.render());
+        return ok(index.render(mydataForm));
+    }
+
+    public static Result delete() {
+        return ok("感謝カード送信完了");
     }
 }
