5e1dd59779dc5343aee13069e6da1deb08a077fb
User <User@H27-NotePC06>
2015-05-27 18:14:53 +0900

    今月の事例と先月の事例を見れるようにした

--- app/controllers/Bbs.java
+++ app/controllers/Bbs.java
@@ -1,19 +1,23 @@
 package controllers;
 
-import java.util.List;
+import java.util.*;
 import models.ThanksCard;
 import play.*;
 import play.mvc.*;
 import views.html.bbs.*;
 
 public class Bbs extends Controller {
+	public static Calendar cal=Calendar.getInstance();
 
     public static Result index() {
-    	List<ThanksCard> card=ThanksCard.find.all();
-        return ok(index.render(card));
+    	List<ThanksCard> thisMonthCard=ThanksCard.find.where().like("helpDate", "%05%").findList();
+        return ok(index.render(thisMonthCard));
     }
+
     public static Result lastMonth() {
-        return ok(lastMonth.render());
+    	//String last="%" + cal.add(Calendar.MONTH, -1) + "%";
+    	List<ThanksCard> lastMonthCard=ThanksCard.find.where().like("helpDate", "%04%").findList();
+        return ok(lastMonth.render(lastMonthCard));
     }
 
 }
