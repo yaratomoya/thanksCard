d9981a2baeb6708e58e8f725ac421aa3b525f25f
User <User@H27-NotePC05>
2015-06-01 15:21:20 +0900

    投稿削除処理

--- app/controllers/ManageBbs.java
+++ app/controllers/ManageBbs.java
@@ -1,14 +1,32 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.*;
 
+import models.ThanksCard;
+import play.*;
+import play.data.Form;
+import play.mvc.*;
 import views.html.managebbs.*;
 
 public class ManageBbs extends Controller {
 
+
+
     public static Result index() {
-        return ok(index.render());
+		Form<ThanksCard> managebbsForm=Form.form(ThanksCard.class);
+    	List<ThanksCard> card=ThanksCard.find.where().eq("deleteRequest",1).findList();
+        return ok(index.render(card,managebbsForm));
+
     }
+
+    public static Result delete(Long cardID){
+
+    	ThanksCard card = ThanksCard.find.byId(cardID);
+    	card.deleteRequest = 2;
+    	card.update();
+
+    	return redirect(routes.ManageBbs.index());
+    }
+
 
 }
